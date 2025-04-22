01d4b9eb18ecf978bbe834e0f9f5be7ef2ddd8c1
Mayas AlKhateeb <alkhateebmayas997@gmail.com>
2025-04-22 15:21:42 +0300

    Adjusted Service

--- BusinessLogic/Services/CustomerAuthService.cs
+++ BusinessLogic/Services/CustomerAuthService.cs
@@ -25,7 +25,34 @@ public class CustomerAuthService : ICustomerAuthService
     }
     public async Task<IdentityResult> RegisterAsync(CustomerRegisterDTO dto)
     {
-        throw new NotImplementedException();
+        var user = new User
+        {
+            UserName = dto.Email,
+            Email = dto.Email,
+        };
+
+        // Create with password in one call
+        var result = await _userManager.CreateAsync(user, dto.Password);
+    
+        if (!result.Succeeded)
+            return result;
+        // Now create the Customer profile
+        Customer newCustomer = new Customer
+        {
+            UserId = user.Id, // Use the generated ID
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            PhoneNumber = dto.PhoneNumber,
+            Whatsapp = dto.Whatsapp,
+            Country = dto.Country
+        };
+
+        await _Repository.AddAsync(newCustomer);
+        await _Repository.SaveAsync();
+
+        // await _userManager.AddToRoleAsync(user, "Customer");
+        
+        return result;
     }
     public Task LogoutAsync()
     {
