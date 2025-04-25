64e6598824b0d2c9c8c514436f7b527fa4f2c615
Mayas AlKhateeb <alkhateebmayas997@gmail.com>
2025-04-25 12:16:00 +0300

    Restored Deleted Files

---
+++ TourismAgency/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+public class RegisterViewModel
+{
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [Display(Name = "Address")]
+    public string Address { get; set; }
+
+    [Required]
+    [Display(Name = "Age")]
+    [Range(10, 100, ErrorMessage = "Age must be between 10 and 100")]
+    public int? Age { get; set; } // Nullable حتى لا يُطلب إلا للطلاب
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    [StringLength(100, ErrorMessage = "Password must be at least 6 characters long.", MinimumLength = 6)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    public string ConfirmPassword { get; set; }
+
+    [Required]
+    [Display(Name = "Role")]
+    public string Role { get; set; }
+
+    // قائمة الاختيارات للـ Roles مثل (Admin, Teacher, Student)
+    public IEnumerable<SelectListItem>? Roles { get; set; }
+}
