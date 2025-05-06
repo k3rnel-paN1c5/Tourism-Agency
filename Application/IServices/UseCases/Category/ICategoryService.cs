4219deaf4f7bd9b455667453345eee19b94f8e8d
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-05-06 11:58:53 +0300

    added Category Service

---
+++ Application/IServices/UseCases/Category/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IServices.UseCases.Category
+{
+    public interface ICategoryService
+    {
+        public Task<GetCategoryDTO> CreateCategoryAsync(CreateCategoryDTO dto);
+        public Task UpdateCategoryAsync(UpdateCategoryDTO dto);
+        public Task DeleteCategoryAsync(int id);
+        Task<IEnumerable<GetCategoryDTO>> GetAllCategoriesAsync();
+        Task <GetCategoryDTO> GetCategoryByIdAsync(int id);
+        
+
+    }
+}
