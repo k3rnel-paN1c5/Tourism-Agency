680fa828c45a10d7ad6347b5fc2103ab667af3c8
Mayas Alkhateeb <121313953+k3rnel-paN1c5@users.noreply.github.com>
2025-05-19 07:48:50 +0300

    Update Program.cs
    Us  "default" as the name of  the connection  string

--- TourismAgency/Program.cs
+++ TourismAgency/Program.cs
@@ -23,12 +23,6 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<IdentityAppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
-
-builder.Services.AddDbContext<TourismAgencyDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("TourismAgencyConnection")));
-
 // Add services to the container.
 
 // Controllers and Views
