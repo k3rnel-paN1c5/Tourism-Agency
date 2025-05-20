600aa84fef2f3c1512437e26f7eecccb2dda0d64
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-05-20 14:57:59 +0300

    fixed CarBooking mapping profile

--- Application/MappingProfiles/CarBookingProfile.cs
+++ Application/MappingProfiles/CarBookingProfile.cs
@@ -13,11 +13,43 @@ namespace Application.MappingProfiles
     {
         public CarBookingProfile() {
 
-            CreateMap<CarBooking, GetCarBookingDTO>();
-            CreateMap<CreateCarBookingDTO, CarBooking>();
-            CreateMap<UpdateCarBookingDTO, CarBooking>();
+            CreateMap<CarBooking, GetCarBookingDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.BookingId))
+                .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => src.Booking!.StartDate))
+                .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => src.Booking!.EndDate))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Booking!.Status))
+                .ForMember(dest => dest.NumOfPassengers, opt => opt.MapFrom(src => src.Booking!.NumOfPassengers))
+                .ForMember(dest => dest.CustomerId, opt => opt.MapFrom(src => src.Booking!.CustomerId))
+                .ForMember(dest => dest.EmployeeId, opt => opt.MapFrom(src => src.Booking!.EmployeeId))
+                .ForMember(dest => dest.CarId, opt => opt.MapFrom(src => src.CarId))
+                .ForMember(dest => dest.WithDriver, opt => opt.MapFrom(src => src.WithDriver))
+                .ForMember(dest => dest.PickUpLocation, opt =>opt.MapFrom(src =>src.PickUpLocation))
+                .ForMember(dest => dest.DropOffLocation, opt=>opt.MapFrom(src =>src.DropOffLocation))
+            .ReverseMap();
 
-        
+
+
+            CreateMap<CreateCarBookingDTO, CarBooking>()
+                 .ForMember(dest => dest.BookingId, opt => opt.Ignore())
+                .ForMember(dest => dest.CarId, opt => opt.MapFrom(src => src.CarId))
+                .ForMember(dest => dest.WithDriver, opt => opt.MapFrom(src => src.WithDriver))
+                .ForMember(dest => dest.PickUpLocation, opt => opt.MapFrom(src => src.PickUpLocation))
+                .ForMember(dest => dest.DropOffLocation, opt => opt.MapFrom(src => src.DropOffLocation))
+                .ForMember(dest => dest.Booking, opt => opt.Ignore())
+                .ForMember(dest =>dest.Car, opt=> opt.Ignore())
+                .ForMember(dest =>dest.ImageShots, opt=>opt.Ignore());
+
+           
+           CreateMap<UpdateCarBookingDTO, CarBooking>()
+             .ForMember(dest => dest.BookingId, opt => opt.Ignore())
+                .ForMember(dest => dest.CarId, opt => opt.MapFrom(src => src.CarId))
+                .ForMember(dest => dest.WithDriver, opt => opt.MapFrom(src => src.WithDriver))
+                .ForMember(dest => dest.PickUpLocation, opt => opt.MapFrom(src => src.PickUpLocation))
+                .ForMember(dest => dest.DropOffLocation, opt => opt.MapFrom(src => src.DropOffLocation))
+                .ForMember(dest => dest.Booking, opt => opt.Ignore())
+                .ForMember(dest => dest.Car, opt => opt.Ignore())
+                .ForMember(dest => dest.ImageShots, opt => opt.Ignore());
+
         }
 
     }
