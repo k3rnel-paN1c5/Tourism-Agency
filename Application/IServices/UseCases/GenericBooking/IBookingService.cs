5fc767d4e6494dd6e24d3c3bf9131e557fc03f7b
Mayas AlKhateeb <alkhateebmayas997@gmail.com>
2025-05-26 23:14:41 +0300

    Remove unimportant methods

--- Application/IServices/UseCases/GenericBooking/IBookingService.cs
+++ Application/IServices/UseCases/GenericBooking/IBookingService.cs
@@ -8,11 +8,7 @@ public interface IBookingService
 {
     public Task<GetBookingDTO> CreateBookingAsync(CreateBookingDTO dto);
     public Task UpdateBookingAsync(UpdateBookingDTO dto);
-    public Task ChangeBookingStatusAsync(int id, BookingStatus st);
     public Task ConfirmBookingAsync(int id);
-    public Task DenyBookingAsync(int id);
-    public Task CancelBookingAsync(int id);
-    public Task DeleteBookingAsync(int id);
     Task<IEnumerable<GetBookingDTO>> GetAllBookingsAsync();
     Task<GetBookingDTO> GetBookingByIdAsync(int id);
 }
