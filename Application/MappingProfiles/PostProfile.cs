66a23e6bb9daea34e851089324925cda73554ebf
Mayas AlKhateeb <alkhateebmayas997@gmail.com>
2025-05-27 18:24:03 +0300

    Fix Error

--- Application/MappingProfiles/PostProfile.cs
+++ Application/MappingProfiles/PostProfile.cs
@@ -10,21 +10,12 @@ namespace Application.MappingProfiles
 {
     public class PostProfile : Profile
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        public PostProfile(IHttpContextAccessor httpContextAccessor)
+        public PostProfile()
         {
-            _httpContextAccessor = httpContextAccessor;
 
-            var userId = _httpContextAccessor
-                    .HttpContext
-                    .User.Claims
-                    .FirstOrDefault(c => c.Type == "UserId" ||
-                                     c.Type == "sub" ||
-                                     c.Type == ClaimTypes.NameIdentifier)?.Value;
             // Mapping CreatePostDTO to Post
             // Mapping CreatePostDTO to Post
-            CreateMap<CreatePostDTO, Post>()
-            .ForMember(dest => dest.EmployeeId, opt => opt.MapFrom(src => userId)) ;
+            CreateMap<CreatePostDTO, Post>();
 
             // Mapping Post to GetPostDTO
             // Mapping Post to GetPostDTO
