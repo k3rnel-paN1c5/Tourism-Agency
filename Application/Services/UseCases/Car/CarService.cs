0e13cacbd9e8b121a1dba5a83c96f5bc3157c91a
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-06-02 00:15:50 +0300

    Added GetAvailableCars

--- Application/Services/UseCases/Car/CarService.cs
+++ Application/Services/UseCases/Car/CarService.cs
@@ -1,26 +1,29 @@
-﻿using System;
-using Domain.Entities;
-using Application.DTOs.Car;
+﻿using Application.DTOs.Car;
 using Application.IServices.UseCases;
 using AutoMapper;
+using Domain.Entities;
 using Domain.IRepositories;
 
 
 
 namespace Application.Services.UseCases
 {
-    public class CarService: ICarService
+    public class CarService : ICarService
     {
         private readonly IRepository<Car, int> _repo;
         private readonly IMapper _mapper;
         private readonly ICategoryService _categoryService;
+        private readonly ICarBookingService _carBookingService;
+        private readonly ITripPlanService _tripPlanService;
 
-        public CarService(IMapper mapper ,IRepository<Car, int> repo , ICategoryService categoryService)
+        public CarService(IMapper mapper, IRepository<Car, int> repo, ICategoryService categoryService, ICarBookingService carBookingService, ITripPlanService tripPlanService)
         {
 
             _mapper = mapper;
             _repo = repo;
             _categoryService = categoryService;
+            _carBookingService = carBookingService;
+            _tripPlanService = tripPlanService;
         }
 
         public async Task<GetCarDTO> CreateCarAsync(CreateCarDTO dto)
@@ -29,9 +32,9 @@ namespace Application.Services.UseCases
                 ?? throw new Exception($"Category {dto.CategoryId} was not found");
 
             var car = _mapper.Map<Car>(dto);
-            
-            car.Category=_mapper.Map<Category>(category); //remove later if found unnecessary 
-            
+
+            car.Category = _mapper.Map<Category>(category); //remove later if found unnecessary 
+
 
             await _repo.AddAsync(car);
             await _repo.SaveAsync();
@@ -47,7 +50,7 @@ namespace Application.Services.UseCases
 
             var car = _mapper.Map<Car>(dto);
             car.Category = _mapper.Map<Category>(category);
-             _repo.Update(car);
+            _repo.Update(car);
             await _repo.SaveAsync();
 
 
@@ -62,14 +65,14 @@ namespace Application.Services.UseCases
 
         public async Task<IEnumerable<GetCarDTO>> GetAllCarsAsync()
         {
-            var cars= await _repo.GetAllAsync();
+            var cars = await _repo.GetAllAsync();
             return _mapper.Map<IEnumerable<GetCarDTO>>(cars);
 
         }
 
         public async Task<GetCarDTO> GetCarByIdAsync(int id)
         {
-            var car= await _repo.GetByIdAsync(id)
+            var car = await _repo.GetByIdAsync(id)
                 ?? throw new Exception($"Category {id} was not found");
             return _mapper.Map<GetCarDTO>(car);
         }
@@ -82,9 +85,35 @@ namespace Application.Services.UseCases
 
         }
 
-        public Task<IEnumerable<GetCarDTO>> GetAvailableCarsAsync(DateTime startDate, DateTime endDate)
+        public async Task<IEnumerable<GetCarDTO>> GetAvailableCarsAsync(DateTime startDate, DateTime endDate)
         {
-            return GetAllCarsAsync();
+            if (startDate >= endDate)
+            {
+                throw new ArgumentException("Start date must be before end date.");
+            }
+
+            var allCars = await _repo.GetAllAsync();
+            var bookedCarIds = new HashSet<int>();
+
+            var CarBookings = await _carBookingService.GetCarBookingsByDateIntervalAsync(startDate, endDate);
+            foreach (var b in CarBookings)
+            {
+                bookedCarIds.Add(b.CarId);
+            }
+            var TripPlans = await _tripPlanService.GetTripPlansByDateIntervalAsync(startDate, endDate);
+            foreach (var tp in TripPlans)
+            {
+                if (tp.TripPlanCars != null)
+                {
+                    foreach (var tpc in tp.TripPlanCars)
+                    {
+                        bookedCarIds.Add(tpc.CarId);
+                    }
+                }
+            }
+            var availableCars = allCars.Where(car => !bookedCarIds.Contains(car.Id));
+
+            return _mapper.Map<IEnumerable<GetCarDTO>>(availableCars);
         }
     }
 }
