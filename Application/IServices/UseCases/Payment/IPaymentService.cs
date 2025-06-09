3d3b0832eb62d2088377c638f07bf780b3e608c4
ZeinabRm13 <zeinabrostom333@gmail.com>
2025-06-09 23:42:25 +0300

    Add separate validation service

--- Application/IServices/UseCases/Payment/IPaymentService.cs
+++ Application/IServices/UseCases/Payment/IPaymentService.cs
@@ -3,18 +3,78 @@ using Domain.Enums;
 
 namespace Application.IServices.UseCases
 {
+    /// <summary>
+    /// Defines the contract for payment-related business logic operations.
+    /// </summary>
     public interface IPaymentService
     {
-        
+        /// <summary>
+        /// Creates a new payment asynchronously.
+        /// </summary>
+        /// <param name="paymentDto">The payment creation data.</param>
+        /// <returns>The created payment information.</returns>
         Task<ReturnPaymentDTO> CreatePaymentAsync(CreatePaymentDTO paymentDto);
+
+        /// <summary>
+        /// Retrieves all payments asynchronously.
+        /// </summary>
+        /// <returns>A collection of all payments.</returns>
         Task<IEnumerable<ReturnPaymentDTO>> GetAllPaymentsAsync();
+
+        /// <summary>
+        /// Retrieves a payment by its ID asynchronously.
+        /// </summary>
+        /// <param name="paymentId">The ID of the payment to retrieve.</param>
+        /// <returns>The payment information.</returns>
         Task<ReturnPaymentDTO> GetPaymentByIdAsync(int paymentId);
+
+        /// <summary>
+        /// Retrieves a payment by booking ID asynchronously.
+        /// </summary>
+        /// <param name="bookingId">The ID of the booking.</param>
+        /// <returns>The payment information for the specified booking.</returns>
         Task<ReturnPaymentDTO> GetPaymentByBookingIdAsync(int bookingId);
+
+        /// <summary>
+        /// Retrieves all payments by status asynchronously.
+        /// </summary>
+        /// <param name="status">The payment status to filter by.</param>
+        /// <returns>A collection of payments with the specified status.</returns>
         Task<IEnumerable<ReturnPaymentDTO>> GetPaymentsByStatusAsync(PaymentStatus status);
+
+        /// <summary>
+        /// Retrieves detailed information about a payment asynchronously.
+        /// </summary>
+        /// <param name="paymentId">The ID of the payment.</param>
+        /// <returns>Detailed payment information.</returns>
         Task<PaymentDetailsDTO> GetPaymentDetailsAsync(int paymentId);
-        
-        // new integrated methods:
+
+        /// <summary>
+        /// Updates payment information asynchronously.
+        /// </summary>
+        /// <param name="paymentDto">The payment update data.</param>
+        /// <returns>The updated payment information.</returns>
+        Task<ReturnPaymentDTO> UpdatePaymentAsync(UpdatePaymentDTO paymentDto);
+
+        /// <summary>
+        /// Processes a payment transaction asynchronously.
+        /// </summary>
+        /// <param name="processPaymentDto">The payment processing data.</param>
+        /// <returns>The result of the payment processing operation.</returns>
         Task<PaymentProcessResultDTO> ProcessPaymentAsync(ProcessPaymentDTO processPaymentDto);
+
+        /// <summary>
+        /// Processes a refund transaction asynchronously.
+        /// </summary>
+        /// <param name="refundDto">The refund processing data.</param>
+        /// <returns>The result of the refund processing operation.</returns>
         Task<PaymentProcessResultDTO> ProcessRefundAsync(ProcessRefundDTO refundDto);
+
+        /// <summary>
+        /// Cancels a payment asynchronously.
+        /// </summary>
+        /// <param name="paymentId">The ID of the payment to cancel.</param>
+        /// <returns>The updated payment information.</returns>
+        Task<ReturnPaymentDTO> CancelPaymentAsync(int paymentId);
     }
 }
--- Application/IServices/UseCases/PaymentMethod/IPaymentMethodService.cs
+++ Application/IServices/UseCases/PaymentMethod/IPaymentMethodService.cs
@@ -1,18 +1,67 @@
 using Domain.Entities;
-using DTO.PaymentMethod;
+using Application.DTOs.PaymentMethod;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Application.IServices.UseCases
 {
+    /// <summary>
+    /// Defines the contract for payment method-related business logic operations.
+    /// </summary>
     public interface IPaymentMethodService
     {
+        /// <summary>
+        /// Retrieves all payment methods asynchronously.
+        /// </summary>
+        /// <returns>A collection of all payment methods.</returns>
         Task<IEnumerable<ReturnPaymentMethodDTO>> GetAllPaymentMethodsAsync();
+
+        /// <summary>
+        /// Retrieves all active payment methods asynchronously.
+        /// </summary>
+        /// <returns>A collection of active payment methods.</returns>
         Task<IEnumerable<ReturnPaymentMethodDTO>> GetActivePaymentMethodsAsync();
+
+        /// <summary>
+        /// Retrieves a payment method by its ID asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the payment method to retrieve.</param>
+        /// <returns>The payment method information.</returns>
         Task<ReturnPaymentMethodDTO> GetPaymentMethodByIdAsync(int id);
+
+        /// <summary>
+        /// Creates a new payment method asynchronously.
+        /// </summary>
+        /// <param name="dto">The payment method creation data.</param>
+        /// <returns>The created payment method information.</returns>
         Task<ReturnPaymentMethodDTO> CreatePaymentMethodAsync(CreatePaymentMethodDTO dto);
+
+        /// <summary>
+        /// Updates payment method information asynchronously.
+        /// </summary>
+        /// <param name="dto">The payment method update data.</param>
+        /// <returns>The updated payment method information.</returns>
         Task<ReturnPaymentMethodDTO> UpdatePaymentMethodAsync(UpdatePaymentMethodDTO dto);
+
+        /// <summary>
+        /// Deletes a payment method asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the payment method to delete.</param>
+        /// <returns>True if the deletion was successful, otherwise false.</returns>
         Task<bool> DeletePaymentMethodAsync(int id);
+
+        /// <summary>
+        /// Toggles the status of a payment method asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the payment method to toggle.</param>
+        /// <returns>The updated payment method information.</returns>
         Task<ReturnPaymentMethodDTO> TogglePaymentMethodStatusAsync(int id);
+
+        /// <summary>
+        /// Checks if a payment method exists asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the payment method to check.</param>
+        /// <returns>True if the payment method exists, otherwise false.</returns>
+        Task<bool> PaymentMethodExistsAsync(int id);
     }
 }
