f5be240813f5484325969d38a03e33e98ca26eb3
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-06-10 16:00:36 +0300

    Added Car Supervisor dashboard

--- TourismAgencyClient/src/routes/AppRoutes.jsx
+++ TourismAgencyClient/src/routes/AppRoutes.jsx
@@ -2,11 +2,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from '../pages/Customer/AuthPage';
 import CustomerDashboard from '../pages/Customer/CustomerDashboard';
 import TripSupervisorDashboard from '../pages/TripSupervisor/TripSupervisorDashboard';
+import CarSupervisorDashboard from '../pages/CarSupervisor/CarSupervisorDashboard';
 import NotFound from '../pages/NotFound';
 
 import RegionManagementPage from '../pages/TripSupervisor/RegionManagementPage';
 import TripManagementPage from '../pages/TripSupervisor/TripManagementPage';
 import TripPlanManagementPage from '../pages/TripSupervisor/TripPlanManagementPage';
+
+
 
 
 export default function AppRoutes() {
@@ -20,6 +23,8 @@ export default function AppRoutes() {
       <Route path="/trip-supervisor/regions" element={<RegionManagementPage />} />
       <Route path="/trip-supervisor/trips" element={<TripManagementPage />} />
       <Route path="/trip-supervisor/trip-plans" element={<TripPlanManagementPage />} />
+
+      <Route path="/car-dashboard" element={<CarSupervisorDashboard />} />
 
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="*" element={<NotFound />} />
